b77a54913ceca9cc4af72d5b4b09d78db934a5be
Nikoleta <Nikoleta@Nikol-PC>
2013-01-16 23:30:16 +0200

    H klasi texnikos

---
+++ Synergeio/src/Synergeio/Texnikos.java
@@ -0,0 +1,54 @@
+
+package Synergeio;
+
+
+import java.util.Vector;
+
+//afhrhmenh klash texnikos apo thn opoia klhronomoun ta eidh twn texnikwn
+public abstract class Texnikos extends Employee{
+    
+    private Vector<Ergasia> ergasies = new Vector<Ergasia>();  //Vector progrmmatismenwn ergasiwn
+    private Vector<IstorikoErgasiwn> istoriko = new Vector<IstorikoErgasiwn>(); //Vector istorikou ergasiwn
+    
+    //kataskevasths
+    public Texnikos(String onoma, String epitheto, String dieythinsi, String thl, String AT){
+        super(onoma, epitheto, dieythinsi, thl, AT);       
+    }
+    
+    //prostiki programmatismenhs ergasias
+    public void addErgasia(Ergasia e){
+        ergasies.add(e);
+    }
+    
+    //diagrafi programmatismenhs ergasias
+    public void removeErgasia(Ergasia e){
+        ergasies.remove(e);
+    }
+    
+    //get sygkekrimenh ergasia 
+    public Ergasia getErgasia(int i){
+        return ergasies.get(i);
+    }
+    
+    //get oles tis ergasies 
+    public Vector<Ergasia> getErgasies(){
+        return ergasies;
+    }
+    
+    //prosthiki oloklhrwmwnhs ergasias
+    public void addIstorikoErgasiwn(IstorikoErgasiwn h){
+        istoriko.add(h);
+    }
+    
+    //diagrafi oloklhrwmwnhs ergasias
+    public void removeIstorikoErgasiwn(IstorikoErgasiwn h){
+        istoriko.remove(h);
+    }
+    
+    //get syskekrimenh oloklhrwmenh ergasia
+    public IstorikoErgasiwn getIstorikoErgasiwn(int i){
+        return istoriko.get(i);
+    }
+    
+   
+}
