a95ff43820a69fdbea39c2de55fc527332df0273
Nikoleta <Nikoleta@Nikol-PC>
2013-01-16 23:26:10 +0200

    H klasi Employee

---
+++ Synergeio/src/Synergeio/Employee.java
@@ -0,0 +1,13 @@
+
+
+package Synergeio;
+
+//o ypallilos klhronomei apo th Person
+public class Employee extends Person {
+    
+    //kataskevasths
+    public Employee(String onoma, String epitheto, String dieythinsi, String thl, String AT){
+        super(onoma, epitheto, dieythinsi, thl, AT);       
+    }
+
+}
